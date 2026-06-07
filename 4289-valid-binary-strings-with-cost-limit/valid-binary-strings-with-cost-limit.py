3e0d0db621192369fb968828d6177fbddfb06a9d
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-06-07 10:34:24 +0530

    Time: 840 ms (5.04%) | Memory: 99.5 MB (6.13%) - LeetSync

---
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
@@ -0,0 +1,39 @@
+class Solution:
+    def generateValidStrings(self, n: int, k: int) -> list[str]:
+        res=[]
+        def solve(temp):
+            if len(temp)==n:
+                res.append("".join(temp))
+                return
+            
+            
+            temp.append("0")
+            solve(temp)
+            temp.pop()
+        
+            temp.append("1")
+            solve(temp)
+            temp.pop()
+        solve([])
+        final=[]
+
+        for bn in res:
+            ans=0
+            valid=True
+            for i in range(n):
+                if i+1<n and bn[i]=="1" and bn[i+1]=="1":
+                    valid=False
+                    break
+                if bn[i]=="1":
+                    ans+=i
+            print(ans)
+            if valid and ans<=k:
+                final.append(bn)
+        return final
+
+
+
+
+
+
+        
