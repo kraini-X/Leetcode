cc4ded41dd445432fea6db22047a17e59ac1039d
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-07-05 10:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (17.23%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        def solve(n):
+            if n==0:
+                return 0
+            
+            if n==1:
+                return 1
+            
+            return n%10+solve(n//10)
+        if num<10:
+            return num
+
+        return self.addDigits(solve(num))
+        
+        
