bc5fb8e30b9a18acde0e349aeb39793d1f3e7cb6
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-07-14 11:22:02 +0530

    Time: 330 ms (25.80%) | Memory: 20.5 MB (53.68%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -3,24 +3,26 @@ class Solution:
         from collections import deque
         q=deque()
         q.append(beginWord)
-        level=0
+        level=1
         wordlist=set(wordList)
         visited=set()
         visited.add(beginWord)
+
         while q:
             for _ in range(len(q)):
                 word=q.popleft()
 
                 if word==endWord:
-                    return level+1
+                    return level
                 
                 for i in range(len(word)):
-                    for j in range(26):
-                        ch=chr((ord('a') + j))
-                        new=word[:i]+ch+word[i+1:]
-
-                        if new in wordlist and new not in visited:
-                            visited.add(new)
-                            q.append(new)
+                    for j in range(97,123):
+                        newWord=word[0:i]+chr(j)+word[i+1:]
+                        if newWord in wordlist and newWord not in visited:
+                            visited.add(newWord)
+                            q.append(newWord)
             level+=1
         return 0
+
+
+        
