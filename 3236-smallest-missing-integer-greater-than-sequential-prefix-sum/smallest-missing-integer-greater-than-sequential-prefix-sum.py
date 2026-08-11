89b83f2ff8b99a44cab7b44a56242e8315b3b2fd
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-08-11 17:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.89%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        n=len(nums)
+        prefix=[0]*n
+
+        prefix[0]=nums[0]
+        for i in range(1,n):
+            prefix[i]=prefix[i-1]+nums[i]
+        
+        i = 1
+
+        while i < n and nums[i] == nums[i - 1] + 1:
+            i += 1
+        #print(prefix)
+        #print(i)
+        temp=prefix[i-1]
+
+        if temp not in nums:
+            return temp
+        else:
+            while temp+1 in nums:
+                temp+=1
+            return temp+1
