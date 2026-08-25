3f2b54ed2206020af1cca52b9f625e0ed772e600
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-08-25 18:29:55 +0530

    Time: 3 ms (20.84%) | Memory: 19.3 MB (53.35%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        minm=float('inf')
+        for i in range(1,102):
+            val=k*i
+            if val<minm and val not in nums:
+                minm=val
+        return minm
+        
