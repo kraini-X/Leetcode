ad68ed78a92764b7d8e60a60da47022a398718b8
kraini-X <141851250+kraini-X@users.noreply.github.com>
2026-09-02 10:48:46 +0530

    Time: 204 ms (12.22%) | Memory: 31.5 MB (42.31%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -11,18 +11,20 @@ class Solution:
         for i in range(n-1,-1,-1):
             while stack and heights[stack[-1]]>=heights[i]:
                 stack.pop()
+            
             if stack:
                 nse[i]=stack[-1]
             stack.append(i)
-        
         #pse
         stack=[]
         for i in range(n):
             while stack and heights[stack[-1]]>=heights[i]:
                 stack.pop()
+            
             if stack:
                 pse[i]=stack[-1]
             stack.append(i)
+            
         ans=0
         for i in range(n):
             width=nse[i]-pse[i]-1
